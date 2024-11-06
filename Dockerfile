e124d339b94c3899c4e37b7ecac5eaf96b5020f1
Marcela Rosales <marcelarosalesj@gmail.com>
2024-11-06 13:14:45 -0600

    use add feature transform pipeline to the app
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,8 @@ FROM python:3.9-slim
 
 WORKDIR /app
 
-#COPY app/requirements.txt .
 COPY app .
 RUN pip install --no-cache-dir -r requirements.txt
-
 
 EXPOSE 8000
 
--- app/main.py
+++ app/main.py
@@ -1,29 +1,75 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field, conlist
+from pydantic import BaseModel, Field
 import pickle
-import numpy as np
+import pandas as pd
 
 with open("app/model.pkl", "rb") as f:
     model = pickle.load(f)
 
+with open("app/model_pipeline.pkl", "rb") as f:
+    pipeline = pickle.load(f)
+
+
 app = FastAPI()
+column_names = [
+    "Student Age",
+    "Sex",
+    "Graduated High-school Type",
+    "Scholarship Type",
+    "Additional Work",
+    "Regular Artistic/Sports Activity",
+    "Do you have a Partner",
+    "Total Salary",
+    "Transportation",
+    "Accommodation in Cyprus",
+    "Mothers Education",
+    "Fathers Education",
+    "Number of Siblings",
+    "Parental Status",
+    "Mothers Occupation",
+    "Fathers Occupation",
+    "Weekly Study Hours",
+    "Reading Frequency (Non-Scientific)",
+    "Reading Frequency (Scientific)",
+    "Attendance to Seminars",
+    "Impact on Success",
+    "Attendance to Classes",
+    "Preparation to Midterm 1",
+    "Preparation to Midterm 2",
+    "Taking Notes in Classes",
+    "Listening in Classes",
+    "Discussion Improves Success",
+    "Flip-Classroom",
+    "Cumulative GPA Last Semester",
+    "Expected GPA at Graduation",
+    "COURSE ID",
+    # "OUTPUT Grade",
+]
 
 
 class PredictionRequest(BaseModel):
     data: dict[str, str] = Field(
         ...,
-        min_items=62,
-        max_items=62,
-        description="Un diccionario con 62 claves y valores de tipo string representando flotantes",
+        min_items=31,
+        max_items=31,
+        description="Un diccionario con 32 claves de nombre específico y valores string que representan enteros",
     )
 
 
 @app.post("/predict")
 def predict(request: PredictionRequest):
     try:
-        data = np.array([[float(request.data[str(i)]) for i in range(62)]])
+        # Extraer y convertir los valores en el orden definido por column_names
+        # data = np.array([[float(request.data[col]) for col in column_names]])
+        df = pd.DataFrame([{col: float(request.data[col]) for col in column_names}])
+
+        # Aplicar la transformación de característicaspipeline
+        data = pipeline.transform(df)
+
+        # Generar la predicción
         prediction = model.predict(data)[0]
         return {"prediction": prediction}
+
     except KeyError as e:
         raise HTTPException(status_code=400, detail=f"Falta la clave: {str(e)}")
     except ValueError as e:
